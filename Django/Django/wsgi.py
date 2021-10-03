6d64b77263d20000affd61908baf3e6a52e6eab2
React-teaching <e1bz6canabis666.8888799ol@gmail.com>
2021-10-03 21:09:34 +0900

    kendoUI add finished

--- Django/Django/wsgi.py
+++ Django/Django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for R_D_O project.
+WSGI config for Django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'R_D_O.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django.settings')
 
 application = get_wsgi_application()
